5d7c1b97305e6721f52af6a51b96a451fdda035d
Hasitha Adikari <adikari.adikari@mycit.ie>
2026-05-31 10:59:23 +0100

    train all models together

--- src/train_models.py
+++ src/train_models.py
@@ -1,6 +1,16 @@
+import os
+import sys
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
 from train_per_node_gru_model import train as train_gru
 from train_per_node_lstm_model import train as train_lstm
+
 from train_st_gnn_flood_model import train as train_st_gnn
+from train_st_gnn_flood_model_sar import train as train_st_gnn_sar
+from train_st_gnn_dyn_edge import train as train_st_gnn_dyn_edge
+from train_st_gnn_hand_edge import train as train_st_gnn_hand_edge
 
 from utils.common_utils import seed_everything
 from utils.config import load_config
@@ -23,4 +33,8 @@ if __name__ == "__main__":
         for t_out in t_outs:
             train_gru(logger, seed, t_in, t_out, max_epochs)
             train_lstm(logger, seed, t_in, t_out, max_epochs)
+
             train_st_gnn(logger, seed, t_in, t_out, max_epochs)
+            train_st_gnn_sar(logger, seed, t_in, t_out, max_epochs)
+            train_st_gnn_dyn_edge(logger, seed, t_in, t_out, max_epochs)
+            train_st_gnn_hand_edge(logger, seed, t_in, t_out, max_epochs)
